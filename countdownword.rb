0a89d1d0c878a74dc8620d0b4b7cad1f64b1bbf8
Julian Nicholls <juliannicholls29@gmail.com>
2013-12-27 16:06:18 +0000

    Re-factored with Rubocop help

--- countdownword.rb
+++ countdownword.rb
@@ -1,82 +1,76 @@
-#----------------------------------------------------------------------------
 # Hold a countdown candidate word, and its letter map which holds a count of
 # each character it contains
 
 class CountdownWord
-
   # Initialize with the word, defaults to lazy evaluation of the letter map.
-  
+
   def initialize( word, map_word = :dont_map )
     @word = word.downcase
-    @wmap = (map_word == :map) ? letter_map( word ) : nil
+    @wmap = map_word == :map ? letter_map( word ) : nil
   end
 
-  
   # Return whether the current word could be built from the letter map
   # that is passed in. e.g.
   #
   # belabour could be built from 'belbaoura' because
   #
-  #   { b: 2, e: 1, l: 1, a: 1, o: 1, u: 1, r: 1} (map for belabour, see below) matches with
+  #   { b: 2, e: 1, l: 1, a: 1, o: 1, u: 1, r: 1} (map for belabour) matches with
   #   { b: 2, e: 1, l: 1, a: 2, o: 1, u: 1, r: 1} (map for 'belbaoura')
   #
-  # in that the number of every character in the word appears in the candidate lettermap 
-  
+  # in that the number of every character in the word appears in the candidate
+  # lettermap.
+
   def can_be_made_from( cand )
-    # If the current word hasn't been lettermap'ed yet, then do it now and 
+    # If the current word hasn't been lettermap'ed yet, then do it now and
     # store it for next time.
-    
-    @wmap ||= CountdownWord::lettermap( @word )
-    
+
+    @wmap ||= CountdownWord.lettermap( @word )
+
     # Work through the word, checking if the candidate letters could build it.
-    
+
     @wmap.each_key do |let|
-      # If the letter list either doesn't have any of the current letter, 
+      # If the letter list either doesn't have any of the current letter,
       # or not enough, then it can't be made from those letters.
-      
-      return false if cand[let].nil? || cand[let] < @wmap[let]  
+
+      return false if cand[let].nil? || cand[let] < @wmap[let]
     end
-    
+
     true  # To have fallen through here, it must be possible
   end
-  
-  
+
   # Return the word so that it can be easily output via print/puts
-  
+
   def to_s
     @word
   end
-  
-  
-  # Perform a comparison, by decreasing length, then alphabetically.  
+
+  # Perform a comparison, by decreasing length, then alphabetically.
   # See readme.md for an interesting aside about operator <=>
-  
-  def <=>( b )
-    other = b.to_s
+
+  def <=>( other )
+    other = other.to_s
     comp  = other.length - @word.length
-    
+
     if comp == 0        # Same length, sort alphabetically
       return -1 if @word < other
       return 1  if @word > other
     end
-  
+
     # One or other is longer, so return that
-    
+
     comp
   end
-  
-  
+
   # This is the key to the whole thing. It returns a map of the used letters, thus:
   # Word      Map
   # belabour  { b: 2, e: 1, l: 1, a: 1, o: 1, u: 1, r: 1 }
   # resonate  { r: 1, e: 2, s: 1, o: 1, n: 1, a: 1, t: 1 }
-  
+
   def self.lettermap( word )
     lmap = Hash.new( 0 )   # Empty map with automatic 0s
-    
+
     word.each_char { |let| lmap[let] += 1 }         # Count 1 more
-    
+
     lmap         # Return map
   end
-  
 end
--- countdownwordlist.rb
+++ countdownwordlist.rb
@@ -1,57 +1,55 @@
+require './countdownword'
+
 #----------------------------------------------------------------------------
 # Hold a list of countdown words.
 #
-# It is held as a hash of arrays, indexed by the initial letter, so that 
-# searching for candidate words from a set of letters only has to search 
+# It is held as a hash of arrays, indexed by the initial letter, so that
+# searching for candidate words from a set of letters only has to search
 # through the set of words that start with each unique letter.
 #
 # The maximum number of words to search is 87K of the 143K words (61%).
 # if the following letters were chosen: abcdfmpst
-# That's a pretty unlikely set to choose, but it does elicit 7 five letter 
+# That's a pretty unlikely set to choose, but it does elicit 7 five letter
 # words.
 
-require './countdownword'
-
 class CountdownWordList
-
   # Initialise the array from a word list file, defaulting to cwords.txt
   # using magic hash key array creation thing learnt from Ruby Koans
-  
+
   def initialize( filename = nil )
     fn     = filename || 'cwords.txt'
     @words = Hash.new { |hash, key| hash[key] = [] }
-        
+
     File.foreach( fn ) do |line|
       @words[line[0]] << CountdownWord.new( line.chomp )
     end
   end
-  
+
   # Get the list of words that can be built from the passed letters
-  
+
   def words_from( letters )
     # Find the unique letters, and also build the letter map for the letters.
-    
+
     letters.downcase!
     uniqs = letters.split(//).uniq
-    lmap  = CountdownWord::lettermap( letters )
-    
+    lmap  = CountdownWord.lettermap( letters )
+
     # Work through the unique letters, testing each word in the letter lists
     # against the passed letters
-    
-    list = uniqs.map do |let| 
+
+    list = uniqs.map do |let|
       @words[let].select { |word| word.can_be_made_from( lmap ) }
     end
-    
-    # Flatten out the array of arrays resulting from map, and sort by length, 
+
+    # Flatten out the array of arrays resulting from map, and sort by length,
     # longest first
-    
+
     list.flatten.sort
   end
-    
+
   # Show the number of words in each letter section of the hash.
-  
+
   def debug
     @words.each_key { |ltr| printf "%c: %5d\n", ltr.upcase, @words[ltr].length }
   end
-
 end
