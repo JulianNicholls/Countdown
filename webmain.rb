2cf7e72534f12f97557b2e4569c4c37d12721995
Julian Nicholls <juliannicholls29@gmail.com>
2014-06-27 15:25:09 +0100

    Fixed list problem

--- webmain.rb
+++ webmain.rb
@@ -1,15 +1,24 @@
-require './countdownwordlist'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'slim'
 require 'sass'
 require 'json'
 
+require './countdownwordlist'
+
 # Countdown for the Web via Sinatra
 class CountdownApp < Sinatra::Application
   puts 'Loading word list...'
   @list = CountdownWordList.new
 
+  class << self
+    attr_reader :list
+  end
+  
+  def list
+    self.class.list
+  end
+  
   get( '/css/style.css' ) { scss :style }
 
   get '/' do    # Show the form
@@ -18,7 +27,7 @@ class CountdownApp < Sinatra::Application
 
   get '/words/:letters' do    # Return the words as formatted HTML
     start   = Time.now
-    @words  = @list.words_from( params[:letters] )
+    @words  = list.words_from( params[:letters] )
     finish  = Time.now
 
     @time  = format( '%.3f', finish - start )
